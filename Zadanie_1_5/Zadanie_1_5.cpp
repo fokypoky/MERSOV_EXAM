bbedc7cc2e0c4f6959496475b8d75f98f9edd9ca
revesdesdefunts <petuhova805@gmail.com>
2022-02-15 01:05:24 +0300

    Задание 1 вариант 6

---
+++ Zadanie_1_5/Zadanie_1_5.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "A: " << endl;
+	int len_a, len_b;
+	cin >> len_a;
+
+	// проверка подходит ли длина массива
+	if (len_a > 5 || len_a <= 0)
+	{
+		while (len_a > 5 || len_a <= 0)
+		{
+			cout << "Incorrect" << endl;
+			cin >> len_a;
+		}
+	}
+	int* A = new int[len_a];
+
+	cout << "B: " << endl;
+	cin >> len_b;
+	// проверка подходит ли длина массива
+	if (len_b > 5 || len_b <= 0)
+	{
+		while (len_b > 5 || len_b <= 0)
+		{
+			cout << "Incorrect enter" << endl;
+			cin >> len_b;
+		}
+	}
+	int* B = new int[len_b];
+
+	int sum_a = 0, sum_b = 0;
+	// заполнение массива
+	for (int i = 0; i < len_a; i++)
+	{
+		cout << "A" << i << endl;
+		cin >> *(A + i);
+		sum_a += *(A + i);
+	}
+	for (int i = 0; i < len_b; i++)
+	{
+		cout << "B" << i << endl;
+		cin >> *(B + i);
+		sum_b += *(B + i);
+	}
+	// поиск среднего арифметического
+	double sr_a = ((double)sum_a) / ((double)len_a);
+	double sr_b = ((double)sum_b) / ((double)len_b);
+	cout << endl << "Arrays:" << endl;
+	for (int i = 0; i < len_a; i++)
+		cout << *(A + i) << " ";
+	cout << endl;
+	for (int i = 0; i < len_b; i++)
+		cout << *(B + i) << " ";
+	cout << "sr a = " << sr_a << "\n" << "sr b = " << sr_b;
+
+	delete[] A, B;
+
+}
---
+++ Zadanie_1_6/Zadanie_1_6.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "A: " << endl;
+	int len_a, len_b;
+	cin >> len_a;
+
+	// проверка подходит ли длина массива
+	if (len_a > 5 || len_a <= 0)
+	{
+		while (len_a > 5 || len_a <= 0)
+		{
+			cout << "Incorrect" << endl;
+			cin >> len_a;
+		}
+	}
+	int* A = new int[len_a];
+
+	cout << "B: " << endl;
+	cin >> len_b;
+	// проверка подходит ли длина массива
+	if (len_b > 4 || len_b <= 0)
+	{
+		while (len_b > 4 || len_b <= 0)
+		{
+			cout << "Incorrect enter" << endl;
+			cin >> len_b;
+		}
+	}
+	int* B = new int[len_b];
+
+	int a_max, b_max;
+	int a_index, b_index;
+
+	for (int i = 0; i < len_a; i++)
+	{
+		cout << "A" << i << endl;
+		cin >> *(A + i);
+		if (i == 0)
+		{
+			a_max = *A;
+			a_index = 0;
+		}
+		else 
+		{
+			if (a_max < *(A + i))
+			{
+				a_max = *(A + i);
+				a_index = i;
+			}
+		}
+	}
+	for (int i = 0; i < len_b; i++)
+	{
+		cout << "B" << i << endl;
+		cin >> *(B + i);
+		if (i == 0)
+		{
+			b_max = *B;
+			b_index = 0;
+		}
+		else
+		{
+			if (b_max < *(B + i))
+			{
+				b_max = *(A + i);
+				b_index = i;
+			}
+		}
+	}
+	cout << endl;
+	if (a_max > b_max)
+	{
+		for (int i = 0; i < len_a; i++)
+			cout << *(A + i) << " ";
+		cout << endl << "Max a = " << a_max << endl << "Index = " << a_index;
+	}
+	else
+	{
+		for (int i = 0; i < len_b; i++)
+			cout << *(B + i) << " ";
+		cout << endl << "Max b = " << b_max << endl << "Index = " << b_index;
+	}
+
+}
