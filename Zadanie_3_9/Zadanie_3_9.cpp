17e351fc620668564cf1d931e20aa3b41dc60e1a
revesdesdefunts <petuhova805@gmail.com>
2022-02-15 04:44:06 +0300

    Задание 3 вариант 9

---
+++ Zadanie_3_9/Zadanie_3_9.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+template <class T>
+class MyClass {
+private:
+    int Len;
+    T* a, * A;
+public: 
+    MyClass(int len)
+    {
+        Len = len;
+        a = new T[len];
+        A = new T[len];
+    }
+    ~MyClass()
+    {
+        delete[] a, A;
+    }
+    void Set()
+    {
+        for (int i = 0; i < Len; i++)
+        {
+            cout << "Введите значение" << endl;
+            cin >> a[i];
+        }
+        for (int i = 0; i < Len; i++)
+        {
+            A[i] = a[i] / (i + 1);
+        }
+    }
+    void Print()
+    {
+        for (int i = 0; i < Len; i++)
+        {
+            cout << A[i] << " ";
+        }
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int l;
+    cin >> l;
+
+    MyClass <double> mc(l);
+    mc.Set();
+    mc.Print();
+
+
+
+}
